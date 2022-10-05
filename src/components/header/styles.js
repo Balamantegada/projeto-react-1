08a2aab32f7c1d55ff244b859157992feee28a49
Balamantegada <rodrigo.kojack@gmail.com>
2022-10-05 11:27:54 -0300

    a
    a

---
+++ src/components/header/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 150px;
+  text-align: center;
+  background: #7b157a;
+`;
+
+export const Header = styled.h1``;
+
+export const Title = styled.div`
+  padding-top: 20px;
+  color: #fff;
+`;
